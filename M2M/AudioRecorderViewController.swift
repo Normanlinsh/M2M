ce41eb27bf06a198e7e61098fd6f830b9e23099b
Mark Chae <MarkChae@Marks-MacBook-Pro-3.local>
2015-11-16 05:46:15 -0600

    Storyboard+AudioRecorder initial skeleton

--- M2M/AudioRecorderViewController.swift
+++ M2M/AudioRecorderViewController.swift
@@ -7,11 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AudioRecorderViewController: UIViewController {
+    
+    var recorder: AVAudioRecorder!
+    var player:AVAudioPlayer!
+    var meterTimer:NSTimer!
+    var soundFileURL:NSURL!
+    
+    @IBOutlet var Record: UIButton!
+    @IBOutlet var Stop: UIButton!
+    @IBOutlet var Play: UIButton!
+    @IBOutlet var Status: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Stop.enabled = false
+        Play.enabled = false
+        /*setSessionPlayback()
+        askForNotifications()
+        checkHeadphones()*/ //methods used in example
+        
+        func updateAudioMeter(timer:NSTimer) {
+            
+            if recorder.recording {
+                let min = Int(recorder.currentTime / 60)
+                let sec = Int(recorder.currentTime % 60)
+                let s = String(format: "%02d:%02d", min, sec)
+                Status.text = s
+                recorder.updateMeters()
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +47,18 @@ class AudioRecorderViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        recorder = nil
+        player = nil
+    }
+    
+    @IBAction func record(sender: UIButton) {
+        
+    }
+    @IBAction func play(sender: UIButton){
+        
+    }
+    @IBAction func stop(sender: UIButton) {
+        
     }
     
 
