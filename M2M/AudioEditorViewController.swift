050f24d4aa14a454a4e0be70f674ba5e344df846
Mark Chae <MarkChae@dyn-72-33-194-65.uwnet.wisc.edu>
2015-12-06 18:02:25 -0600

    Audio Editor Append method + rearrangement

--- M2M/AudioEditorViewController.swift
+++ M2M/AudioEditorViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import AudioToolbox
-import AVFoundation //Experimenting
+import AVFoundation
 import UIKit
 import Parse
 
@@ -17,13 +17,13 @@ class AudioEditorViewController: UIViewController, AVAudioPlayerDelegate, UITabl
     
     var url:NSURL!
     
-    /*@IBOutlet var Play: UIButton!
+    @IBOutlet var Play: UIButton!
     
     @IBOutlet var Stop: UIButton!
     
     @IBOutlet var Edit: UIButton!
     
-    @IBOutlet var Delete: UIButton!*/
+    @IBOutlet var Delete: UIButton!
     
 
     override func viewDidLoad() {
@@ -54,12 +54,119 @@ class AudioEditorViewController: UIViewController, AVAudioPlayerDelegate, UITabl
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         return cell as UITableViewCell
     }
+    //Slide to delete
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            //.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
     
+    func createTableMenu() {
+        //Create alert/action sheet menu with all files as options
+    }
+    
+    func append(audio1: NSURL, audio2:  NSURL) {
+        
+        let composition = AVMutableComposition()
+        var track1:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio,
+            preferredTrackID: CMPersistentTrackID())
+        var track2:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: CMPersistentTrackID())
+        
+        var documentDirectoryURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! 
+        var fileDestinationUrl = documentDirectoryURL.URLByAppendingPathComponent(" ")
+        print(fileDestinationUrl)
+        
+        
+        var url1 = audio1
+        var url2 = audio2
+        
+        
+        var avAsset1 = AVURLAsset(URL: url1, options: nil)
+        var avAsset2 = AVURLAsset(URL: url2, options: nil)
+        
+        var tracks1 =  avAsset1.tracksWithMediaType(AVMediaTypeAudio)
+        var tracks2 =  avAsset2.tracksWithMediaType(AVMediaTypeAudio)
+        
+        var assetTrack1:AVAssetTrack = tracks1[0]
+        var assetTrack2:AVAssetTrack = tracks2[0]
+        
+        
+        var duration1: CMTime = assetTrack1.timeRange.duration
+        var duration2: CMTime = assetTrack2.timeRange.duration
+        
+        var timeRange1 = CMTimeRangeMake(kCMTimeZero, duration1)
+        var timeRange2 = CMTimeRangeMake(duration1, duration2)
+        
+        do {
+            try track1.insertTimeRange(timeRange1, ofTrack: assetTrack1, atTime: kCMTimeZero)
+            try track2.insertTimeRange(timeRange2, ofTrack: assetTrack2, atTime: duration1)
+        }
+        catch _ {
+        }
+        
+        var assetExport = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)
+        assetExport!.outputFileType = AVFileTypeAppleM4A
+        assetExport!.outputURL = fileDestinationUrl
+        assetExport.exportAsynchronouslyWithCompletionHandler({
+            switch assetExport.status{
+            case  AVAssetExportSessionStatus.Failed:
+                println("Failed \(assetExport.error)")
+            case AVAssetExportSessionStatus.Cancelled:
+                println("Cancelled \(assetExport.error)")
+            default:
+                println("Success")
+            //Add new file to library, unfinished
+            }
+            
+        })
 
+    }
     
+    func overlay(audio1: NSURL, audio2:  NSURL) {
+        
+    }
+    
+    
+    @IBAction func Play(sender: UIButton) {
+        //open table menu...
+        //play item from table menu
+    }
+    
+    @IBAction func Edit(sender: UIButton) {
+        let alert = UIAlertController(title: "Edit", message: nil,
+            preferredStyle: .ActionSheet)
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) {(action) in}
+        alert.addAction(cancel)
+        let append = UIAlertAction(title: "Append", style: .Default) {(action) in
+            //open table menu...
+            //item1=file at index
+            //open table menu again without item1
+            //item2=file at index
+            //call append()
+            //add new item to table
+        }
+        alert.addAction(append)
+        let overlay = UIAlertAction(title: "Overlay", style: .Default) {(action) in
+            //open table menu...
+            //item1=file at index
+            //open table menu again without item1
+            //item2=file at index
+            //call overlay()
+            //add new item to table
+        }
+        alert.addAction(overlay)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func Delete(sender: UIButton) {
+        //open table menu...
+        //play item from table menu
+    }
+
     /*@IBAction func test(sender: AnyObject) {
         do {
             self.player = try AVAudioPlayer(contentsOfURL: url!)
